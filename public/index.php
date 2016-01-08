14a2989ec17a99c9cc86ac8936687a0f5d0baf7d
d0niek <glinka18@wp.pl>
2016-01-08 18:51:51 +0100

    Catch all exceptions

--- public/index.php
+++ public/index.php
@@ -12,9 +12,13 @@ define('ROOT_PATH', dirname(__DIR__));
 define('XML_FILE', ROOT_PATH . '/kolekcja.xml');
 
 if (file_exists(XML_FILE)) {
-    $kernel = new Kernel(XML_FILE);
+    try {
+        $kernel = new Kernel(XML_FILE);
 
-    $kernel->start();
+        $kernel->start();
+    } catch (Exception $e) {
+        echo '<h2>Error:</h2>', $e->getMessage();
+    }
 } else {
     echo 'File <b>' . XML_FILE . '</b> dose not exists!';
 }
