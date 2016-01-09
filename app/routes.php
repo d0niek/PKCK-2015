e7a401a713ec5e9fd081851b639d377730ba19cd
d0niek <glinka18@wp.pl>
2016-01-09 20:13:37 +0100

    Removed unused routes

--- app/routes.php
+++ app/routes.php
@@ -13,21 +13,6 @@ return [
         'action' => 'index',
     ],
     [
-        'route' => 'add-author',
-        'controller' => 'Author',
-        'action' => 'add',
-    ],
-    [
-        'route' => 'edit-author/{id}',
-        'controller' => 'Author',
-        'action' => 'edit',
-    ],
-    [
-        'route' => 'delete-author/{id}',
-        'controller' => 'Author',
-        'action' => 'delete',
-    ],
-    [
         'route' => 'add-record',
         'controller' => 'Record',
         'action' => 'add',
@@ -40,21 +25,6 @@ return [
     [
         'route' => 'delete-record/{id}',
         'controller' => 'Record',
-        'action' => 'delete',
-    ],
-    [
-        'route' => 'add-track',
-        'controller' => 'Track',
-        'action' => 'add',
-    ],
-    [
-        'route' => 'edit-track/{id}',
-        'controller' => 'Track',
-        'action' => 'edit',
-    ],
-    [
-        'route' => 'delete-track/{id}',
-        'controller' => 'Track',
         'action' => 'delete',
     ],
     [
